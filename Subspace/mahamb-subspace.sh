c59605cf590dee6ea644b858b0029c05500a83ec
mahamb <32960011+mahamb@users.noreply.github.com>
2023-11-17 01:36:38 +0800

    Update mahamb-subspace.sh

--- Subspace/mahamb-subspace.sh
+++ Subspace/mahamb-subspace.sh
@@ -21,38 +21,38 @@ if [ -f "$bash_profile" ]; then
 fi
 sleep 1 && curl -s https://raw.githubusercontent.com/mahamb/logo/main/mahamb-logo.sh | bash && sleep 1
 read -p "Enter your farmer/reward address: " SUBSPACE_WALLET
-echo -e "\e[32mYour wallet address:\e[39m $SUBSPACE_WALLET"
+echo -e "\e[33mYour wallet address:\e[39m $SUBSPACE_WALLET"
 echo "=============================================================================================="
 echo ""
 echo "Enter your node name to be identified on the network"
 read -p "Node name should not contain invalid chars such as '.' and '@'(press enter to use the default: 'MahambNodes'): " SUBSPACE_NODENAME
 SUBSPACE_NODENAME=${SUBSPACE_NODENAME:-MahambNodes}
-echo -e "\e[32mYour node name:\e[39m $SUBSPACE_NODENAME"
+echo -e "\e[33mYour node name:\e[39m $SUBSPACE_NODENAME"
 echo "=============================================================================================="
 echo ""
 read -p "Specify a path for storing farm files (press enter to use the default: `echo $HOME/.local/share/subspace-farmer`): " SUBSPACE_FARM_PATH
 SUBSPACE_FARM_PATH=${SUBSPACE_FARM_PATH:-$HOME/.local/share/subspace-farmer}
-echo -e "\e[32mYour path for storing farm files\e[39m: $SUBSPACE_FARM_PATH"
+echo -e "\e[33mYour path for storing farm files\e[39m: $SUBSPACE_FARM_PATH"
 echo "=============================================================================================="
 echo ""
 read -p "Specify a path for storing node files (press enter to use the default: `echo $HOME/.local/share/subspace-node`): " SUBSPACE_NODE_PATH
 SUBSPACE_NODE_PATH=${SUBSPACE_NODE_PATH:-$HOME/.local/share/subspace-node}
-echo -e "\e[32mYour path for storing node files:\e[39m $SUBSPACE_NODE_PATH"
+echo -e "\e[33mYour path for storing node files:\e[39m $SUBSPACE_NODE_PATH"
 echo "=============================================================================================="
 echo ""
 echo "Specify a farm size (defaults to '2.0 GB', press enter to use the default) "
 read -p "Example value(don't use quotes) '2.0 GB' '2GB' '2 GB' '2G' '2 G' '2T' '2 T' '2TB' '2 TB': " PLOT_SIZE
 PLOT_SIZE=${PLOT_SIZE:-2GB}
-echo -e "\e[32mYour plot size is:\e[39m $PLOT_SIZE"
+echo -e "\e[33mYour plot size is:\e[39m $PLOT_SIZE"
 echo "=============================================================================================="
 sudo mkdir -p $SUBSPACE_FARM_PATH
 sudo mkdir -p $SUBSPACE_NODE_PATH
 echo ""
-echo -e "\e[32mYour plot size is:\e[39m $PLOT_SIZE"
-echo -e "\e[32mYour node name:\e[39m $SUBSPACE_NODENAME"
-echo -e "\e[32mYour wallet address:\e[39m $SUBSPACE_WALLET"
-echo -e "\e[32mYour path for storing node files:\e[39m $SUBSPACE_NODE_PATH"
-echo -e "\e[32mYour path for storing farm files:\e[39m $SUBSPACE_FARM_PATH"
+echo -e "\e[33mYour plot size is:\e[39m $PLOT_SIZE"
+echo -e "\e[33mYour node name:\e[39m $SUBSPACE_NODENAME"
+echo -e "\e[33mYour wallet address:\e[39m $SUBSPACE_WALLET"
+echo -e "\e[33mYour path for storing node files:\e[39m $SUBSPACE_NODE_PATH"
+echo -e "\e[33mYour path for storing farm files:\e[39m $SUBSPACE_FARM_PATH"
 echo ""
 sleep 3
 
@@ -115,20 +115,20 @@ sleep 120
 sudo systemctl restart subspaced-farmer
 
 echo "==================================================="
-echo -e '\n\e[42mChecking node status\e[0m\n' && sleep 1
+echo -e '\n\e[33mChecking node status\e[0m\n' && sleep 1
 if [[ `service subspaced status | grep active` =~ "running" ]]; then
   echo -e "Your Subspace node has been \e[32minstalled and running\e[39m!"
   echo -e "You can check node status by issuing the command \e[7mservice subspaced status\e[0m"
   echo -e "Press \e[7mQ\e[0m for exit from status menu"
 else
-  echo -e "Your Subspace node \e[31mwas not installed correctly\e[39m, please reinstall."
+  echo -e "Your Subspace node \e[41mwas not installed correctly\e[39m, please reinstall."
 fi
 echo "==================================================="
-echo -e '\n\e[42mChecking farmer status\e[0m\n' && sleep 1
+echo -e '\n\e[33mChecking farmer status\e[0m\n' && sleep 1
 if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
   echo -e "Your Subspace farmer has been \e[32minstalled and works\e[39m!"
   echo -e "You can check node status by the issung command \e[7mservice subspaced-farmer status\e[0m"
   echo -e "Press \e[7mQ\e[0m to exit from status menu"
 else
-  echo -e "Your Subspace farmer \e[31mwas not installed correctly\e[39m, please reinstall."
+  echo -e "Your Subspace farmer \e[41mwas not installed correctly\e[39m, please reinstall."
 fi
