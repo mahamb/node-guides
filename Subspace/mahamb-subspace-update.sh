42c6f1f4d1a6e10724b619233bca095672f6fd87
mahamb <32960011+mahamb@users.noreply.github.com>
2023-11-17 01:33:17 +0800

    Update mahamb-subspace-update.sh

--- Subspace/mahamb-subspace-update.sh
+++ Subspace/mahamb-subspace-update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sleep 1 && curl -s https://raw.githubusercontent.com/mahamb/logo/main/mahamb-logo.sh | bash && sleep 3
 echo "==================================================="
-echo -e '\n\e[42mStopping Subspace Farmer Service....\e[0m\n' && sleep 1
+echo -e '\n\e[33mStopping Subspace Farmer Service....\e[0m\n' && sleep 1
 cd $HOME
 systemctl stop subspaced-farmer
-echo -e '\n\e[42mStopping Subspace Node Service....\e[0m\n' && sleep 1
+echo -e '\n\e[33mStopping Subspace Node Service....\e[0m\n' && sleep 1
 sleep 120
 systemctl stop subspaced
 
-echo -e '\n\e[42mDeleting the old subspace bin files....\e[0m\n' && sleep 1
+echo -e '\n\e[33mDeleting the old subspace bin files....\e[0m\n' && sleep 1
 rm -rf /usr/local/bin/*
 
-echo -e '\n\e[42mDownloading the loading subspace bin files....\e[0m\n' && sleep 1
+echo -e '\n\e[33mDownloading the loading subspace bin files....\e[0m\n' && sleep 1
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
 
@@ -21,19 +21,19 @@ sudo mv subspace-farmer /usr/local/bin/
 
 sleep 5
 
-echo -e '\n\e[42mStarting the Subspace....\e[0m\n' && sleep 3
+echo -e '\n\e[33mStarting the Subspace....\e[0m\n' && sleep 3
 systemctl start subspaced
 sleep 120
 systemctl start subspaced-farmer
 
 echo "==================================================="
-echo -e '\n\e[42mChecking node status\e[0m\n' && sleep 1
+echo -e '\n\e[33mChecking node status\e[0m\n' && sleep 1
 if [[ `systemctl status subspaced | grep active` =~ "running" ]]; then
   echo -e "Your Subspace node has been \e[32mupdated and running\e[39m!"
   echo -e "You can check node status by issuing the command \e[7msystemctl status subspaced\e[0m"
   echo -e "Press \e[7mQ\e[0m for exit from status menu"
 else
-  echo -e "Your Subspace node \e[31mwas not installed correctly\e[39m, please reinstall."
+  echo -e "Your Subspace node \e[31mwas not installed correctly\e[41m, please reinstall."
 fi
 echo "==================================================="
 echo -e '\n\e[42mChecking farmer status\e[0m\n' && sleep 1
@@ -42,6 +42,6 @@ if [[ `systemctl status subspaced-famer | grep active` =~ "running" ]]; then
   echo -e "You can check node status by the issung command \e[7msystemctl status subspaced-famers\e[0m"
   echo -e "Press \e[7mQ\e[0m to exit from status menu"
 else
-  echo -e "Your Subspace farmer \e[31mwas not installed correctly\e[39m, please reinstall or restart your farmer service."
-  echo -e "Restart your farmer service using command:   systemctl restart subspaced-farmer"
+  echo -e "Your Subspace farmer \e[31mwas not installed correctly\e[41m, please reinstall or restart your farmer service."
+  echo -e "Restart your farmer service using command:   \e[7msystemctl restart subspaced-farmer\e[0m"
 fi
