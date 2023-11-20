90db391001c102f335cba75348fad5145c8e127e
mahamb <32960011+mahamb@users.noreply.github.com>
2023-11-20 21:38:42 +0800

    Update mahamb-subspace.sh

--- Subspace/mahamb-subspace.sh
+++ Subspace/mahamb-subspace.sh
@@ -65,8 +65,10 @@ rm -rf subspace-node subspace-farmer
 #wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-13/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-13
 #wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-14/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-14
 #wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-14/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-14
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
+#wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
+#wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-15/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-15
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-19/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-19
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-19/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-19
 
 sudo chmod +x subspace-node subspace-farmer 
 sudo mv subspace-node /usr/local/bin/
